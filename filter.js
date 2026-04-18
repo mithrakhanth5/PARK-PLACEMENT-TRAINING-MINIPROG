b25d34f6d1298345ee0d1b1a536ceb66fe2ff729
sivak <sivak@example.com>
2026-04-18 14:47:39 +0530

    Keep only HTML files in repository

--- filter.js
+++
@@ -1,4 +0,0 @@
-//filter
-let numbers2 = [1, 2, 3, 4, 5];
-let evenNumbers = numbers2.filter(num => num % 2 === 0);
-console.log(evenNumbers);
--- map.js
+++
@@ -1,7 +0,0 @@
-//map
-let numbers2 = [1, 2, 3, 4, 5];
-let squaredNumbers = numbers2.map(num => num * num);
-console.log(squaredNumbers);
-
-//for each
-numbers2.forEach(num => console.log("ForEach: " + num));
